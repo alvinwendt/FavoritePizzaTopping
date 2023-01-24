fcefd60fc290a9136dc943cb238fed34a9d2cc2d
alvinwendt <alvinwendt@gmail.com>
2023-01-24 10:53:54 -0600

    Add project files.

---
+++ FavoritePizzaTopping/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Numerics;
+using System.Text.Json;
+
+namespace FavoritePizzaTopping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Pizza Topping App");
+
+            // read file into a string and deserialize JSON to a type
+            List<string> pizza = JsonConvert.DeserializeObject<List<string>>(@"C:\Users\alvin\source\repos\FavoritePizzaTopping\pizzas.json");
+
+            Console.WriteLine(pizza);
+        }
+    }
+
+
+
+}
